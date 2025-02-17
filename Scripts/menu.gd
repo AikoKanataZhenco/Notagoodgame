6d27b018ed277cc6cd4dbaae81a76e13c62253cc
Mizuraaa <98308592+Mizuraa@users.noreply.github.com>
2025-02-17 22:12:05 +0800

    Add files via upload

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,6 +3,6 @@ extends Control
 func _ready(): pass
 
 func _on_start_pressed():
-	Loader.change_level("res://Scene/HouseInt1.tscn")
+	Loader.change_level("res://Scene/House.tscn")
 
 func _on_quit_pressed() -> void: get_tree().quit()
